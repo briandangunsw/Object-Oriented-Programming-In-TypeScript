3766bb357308ca21a5a0a5ea2efe600af83e40ee
Brian Dang <long.dang@student.unsw.edu.au>
2025-05-19 03:47:07 +1000

    finished objects part

---
+++ objects.js
@@ -0,0 +1,27 @@
+/*
+const name = "Mario"
+const speed = 1
+const isInvincible = true
+
+const hasFireAbility = true;
+
+export {};
+*/
+var Player = /** @class */ (function () {
+    function Player() {
+    }
+    Player.prototype.greet = function () {
+        console.log("Hello, my name is ".concat(this.name));
+    };
+    return Player;
+}());
+var mario = new Player();
+mario.name = "Mario";
+mario.health = 10;
+mario.isInvincible = true;
+var peach = new Player();
+peach.name = "Peach";
+peach.health = 8;
+peach.isInvincible = true;
+mario.greet();
+peach.greet();
---
+++ objects.ts
@@ -0,0 +1,33 @@
+/*
+const name = "Mario"
+const speed = 1
+const isInvincible = true
+
+const hasFireAbility = true;
+
+export {};
+*/
+
+class Player {
+  name: string;
+  health: number;
+  isInvincible: boolean;
+  crush: Player;
+
+  greet() {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+}
+
+const mario = new Player();
+mario.name = "Mario"
+mario.health = 10
+mario.isInvincible = true;
+
+const peach = new Player()
+peach.name = "Peach"
+peach.health = 8;
+peach.isInvincible = true;
+
+mario.greet()
+peach.greet()
